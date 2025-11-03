da05ea052eb1317030ad986e6f9a0ceeb6f46f88
Erin Johnston <erin.johnstonno.4@gmail.com>
2025-11-03 10:41:39 -0800

    Update bst.py

--- bst.py
+++ bst.py
@@ -1,5 +1,29 @@
 import sys
 import unittest
 from typing import *
+from typing import Any, Callables
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6) 
+
+
+
+############### Data Definitons ###################
+
+BinTree : TypeAlias = Union["Node", None]
+
+@dataclass(frozen = True)
+class Node:
+    first: Any
+    rest: BinTree
+
+@dataclass(frozen = True)
+class BinarySearchTree:
+    fun : "comes_before"
+    tree : BinTree
+
+############ Functions ######################
+
+
+## determine if one value comes before another
+def comes_before(prim: int, sec: int) -> bool:
+    pass
