f8824bcd083eb5f154726cbecaa16d897fb3b000
Erin Johnston <erin.johnstonno.4@gmail.com>
2025-11-07 09:16:26 -0800

    insert and lookup functions
    finally got help with helper function code for insert and lookup

--- bst.py
+++ bst.py
@@ -1,7 +1,7 @@
 import sys
 import unittest
 from typing import *
-from typing import Any, Callables
+from typing import Any, Callable
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6) 
 
@@ -13,17 +13,72 @@ BinTree : TypeAlias = Union["Node", None]
 
 @dataclass(frozen = True)
 class Node:
-    first: Any
-    rest: BinTree
+    value: Any
+    left: BinTree
+    right : BinTree
 
 @dataclass(frozen = True)
 class BinarySearchTree:
-    fun : "comes_before"
+    fun : Callable[[Any,Any], bool]
     tree : BinTree
 
-############ Functions ######################
+################ Functions ######################
 
 
 ## determine if one value comes before another
 def comes_before(prim: int, sec: int) -> bool:
-    pass
+    if prim < sec:
+        return True
+    else:
+        return False
+
+#### IS EMPTY ####    
+## determine if BST is empty or not
+def is_empty(bst: BinarySearchTree) -> bool:
+    match bst:
+        case BinarySearchTree(_, tree):
+            '''return tree is None''' # another way of writing the next four lines
+            if tree is None:
+                return True
+            else:
+                return False
+        
+
+
+#### INSERT ####
+
+## insert helper function
+def insert_helper(t: BinTree, val: Any, lt: Callable[[Any, Any], bool]) -> BinTree:
+    if t is None:
+        return Node(val, None, None)
+    elif lt(val, t.value):
+        return Node(t.value, insert_helper(t.left, val, lt), t.right)
+    else:
+        return Node(t.value, t.left, insert_helper(t.right, val, lt))
+
+## insert value into BinarySearchTree
+def insert(bst: BinarySearchTree, val : Any) -> BinarySearchTree:
+    return BinarySearchTree(bst.fun, insert_helper(bst.tree, val, bst.fun))
+
+
+#### LOOKUP ####
+
+## lookup helper function
+
+def is_equal(a: Any, b: Any, lt: Callable[[Any, Any], bool]) -> bool:
+    return (not lt(a,b)) and (not lt(b, a))
+
+def lookup_helper(t: BinTree, val: Any, lt: Callable[[Any, Any], bool]) -> bool:
+    if t is None:
+        return False
+    elif is_equal(val, t.value, lt):
+        return True
+    elif lt(val, t.value):
+        return lookup_helper(t.left, val, lt)
+    else:
+        return lookup_helper(t.right, val, lt)
+
+## determine if value is stored in a BinarySearchTree
+def lookup(bst: BinarySearchTree, val: Any) -> bool:
+    return lookup_helper(bst.tree, val, bst.fun)
+
--- bst_tests.py
+++ bst_tests.py
@@ -7,9 +7,19 @@ from bst import *
 
 
 class BSTTests(unittest.TestCase):
- def test_example(self):
- pass
+    def test_comes_before(self):
+        self.assertTrue(comes_before(2, 7))
+    def test_comes_befoe2(self):
+        self.assertFalse(comes_before(3, 0))
+    def test_is_empty(self):
+        bst = BinarySearchTree(comes_before, None)
+        self.assertTrue(is_empty(bst))
+    def test_is_empty2(self):
+        pass
+
+
+    
 
 
 if (__name__ == '__main__'):
- unittest.main() 
+   unittest.main() 
